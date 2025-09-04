133686da73c25f04044683c6fde569b0d428c1cf
Justin <chongj271@gmail.com>
2025-09-05 03:00:43 +0800

    first version without loops

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,20 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Game {
 
 
     public static void main(String[] args){
+
+        System.out.println("Guess the number");
+        Random r = new Random();
+        int num = r.nextInt(101);
+        Scanner sc = new Scanner(System.in);
+        int guess = sc.nextInt();
+        if (guess > num)System.out.println("Too high");
+        else if (guess < num)System.out.println("Too low");
+        else System.out.println("Correct, the number is" + num);
+
 
     }
 }
